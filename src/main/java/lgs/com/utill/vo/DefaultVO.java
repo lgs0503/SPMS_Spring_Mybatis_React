bde7fef6f109ab6931403ad07ffdc770f9e4ced2
lgs0503 <tjrdl0503@naver.com>
2022-01-24 20:43:46 +0900

    [백엔드] DefaultVO 생성자 기본값 지정 User : admin Date : 현재날짜

--- src/main/java/lgs/com/utill/vo/DefaultVO.java
+++ src/main/java/lgs/com/utill/vo/DefaultVO.java
@@ -2,6 +2,9 @@ package lgs.com.utill.vo;
 
 import lombok.Data;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Data
 public class DefaultVO {
 
@@ -10,4 +13,16 @@ public class DefaultVO {
 	private String updateDate;
 	private String updateUser;
 
+	/* 기본값 초기화 생성자 */
+	public DefaultVO(){
+		long currTime = System.currentTimeMillis();
+
+		SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+		this.createUser = "admin";
+		this.updateUser = "admin";
+
+		this.createDate = timeFormat.format(new Date(currTime));
+		this.updateDate = timeFormat.format(new Date(currTime));
+	}
 }
