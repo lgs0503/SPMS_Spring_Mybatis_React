172da6714b0107e7a28e41cdf52b5afd41fe4f96
lgs0503 <tjrdl0503@naver.com>
2022-02-05 15:46:57 +0900

    [프론트] 공통 쿠키 추가 읽기 삭제 추가

--- docs/js/comm.js
+++ docs/js/comm.js
@@ -26,7 +26,7 @@ function fetchLoad(url, method, data, callback){
 /**
  * Null Check 함수 
  * @param data : NULL체크할데이터
- * @return Null 아니면 TRUE NULL 이면 FALSE
+ * @return Null아니면TRUENull이면FALSE
  * */
 function nullCheck(data) {
     if (data == null || data == "" || data == "undefined"){
@@ -34,4 +34,46 @@ function nullCheck(data) {
     } else {
         return true;
     }
+}
+/**
+ * 쿠키 저장하기
+ * @param cookieName : 쿠키이름
+ * @param value : 쿠키값
+ * @param exdays : 쿠키저장날짜몇일
+ * */
+function setCookie(cookieName, value, exdays) {
+    let exdate = new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    let cookieValue = escape(value)
+        + ((exdays == null) ? "" : "; expires=" + exdate.toGMTString());
+    document.cookie = cookieName + "=" + cookieValue;
+}
+/**
+ * 쿠키 삭제
+ * @param cookieName : 삭제할쿠키이름
+ * */
+function deleteCookie(cookieName) {
+    let expireDate = new Date();
+    expireDate.setDate(expireDate.getDate() - 1);
+    document.cookie = cookieName + "= " + "; expires="
+        + expireDate.toGMTString();
+}
+/**
+ * 쿠키 가져오기
+ * @param cookieName : 가져올쿠키이름
+ * */
+function getCookie(cookieName) {
+    cookieName = cookieName + '=';
+    let cookieData = document.cookie;
+    let start = cookieData.indexOf(cookieName);
+    let cookieValue = '';
+    if (start != -1) { // 쿠키가 존재하면
+        start += cookieName.length;
+        let end = cookieData.indexOf(';', start);
+        if (end == -1) // 쿠키 값의 마지막 위치 인덱스 번호 설정
+            end = cookieData.length;
+        console.log("end위치  : " + end);
+        cookieValue = cookieData.substring(start, end);
+    }
+    return unescape(cookieValue);
 }
