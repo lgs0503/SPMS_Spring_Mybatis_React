af919a97bc6d6e56a96546b1977124e2ac65a222
lgs0503 <tjrdl0503@naver.com>
2022-02-10 22:15:58 +0900

    [프론트] gitpage 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react";
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import {Routes, Route, BrowserRouter, HashRouter} from 'react-router-dom';
 import AdminLayout from './component/admin/index';
 import AdminLogin from './component/admin/login';
 import AdminRegister from './component/admin/register';
@@ -8,14 +8,20 @@ import Main from './component/client/index';
 
 const App = () =>{
   return (
-      <BrowserRouter basename="/spring-showpingmall">
+
+      <HashRouter>
           <Routes>
-              <Route path="/"           element={<Main />} />
-              <Route path="/admin/*"     element={<AdminLayout />} />
-              <Route path="/admin/login"     element={<AdminLogin />} />
+              <Route path="/"                   element={<Main />} />
+              <Route path="/admin/*"            element={<AdminLayout />} />
+              <Route path="/admin/login"        element={<AdminLogin />} />
               <Route path="/admin/register"     element={<AdminRegister />} />
           </Routes>
+      </HashRouter>
+
+      /* git Page 에 배포할때는 못씀 
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
       </BrowserRouter>
+      */
   );
 }
 
