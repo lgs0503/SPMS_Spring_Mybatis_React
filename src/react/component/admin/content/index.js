1c21b6bcdcfbbc9df75ec0feee6ee733d9a2af2e
lgs0503 <tjrdl0503@naver.com>
2022-03-15 13:04:03 +0900

    [프론트] 메인화면 공지 자유게시판 조회

--- src/react/component/admin/content/index.js
+++ src/react/component/admin/content/index.js
@@ -1,17 +1,61 @@
-import React, {useEffect} from 'react';
-import AdminDashboardCard from "./dashboardcrad";
+import React, {useEffect, useState} from 'react';
+import BoardCard from "./boardCard";
 import Table from "../../common/Table";
 import * as common from "../../../comm/common";
 
 const  AdminMain = () => {
-    useEffect(() => {
 
+    const [bodyNoticeData, setBodyNoticeData] = useState(null);
+    const [bodyFreeBoardData, setBodyFreeBoardData] = useState(null);
+
+
+    let headerColData = [{title: "ID",         name : "postId",             width:"10px",  hidden: false, useData : true}
+                        ,{title: "게시글제목",    name : "postTitle",        width:"50%",   hidden: false, useData : true}
+                        ,{title: "작성자",    name : "createUser",           width:"30%",   hidden: false, useData : false}
+                        ,{title: "작성일",    name : "createDate",           width:"25%",   hidden: false, useData : false}];
+
+    let tableInitNotice = {
+        headerColData : headerColData
+        ,   selectCol : 'postId'
+        ,   counted : false
+        ,   title : 'Notice List'
+        ,   maxData : 5
+        ,   colSpan : 4
+        ,   cellSelectEvent : (e) => {
+        }
+    };
+
+    let tableInitFreeBoard = {
+        headerColData : headerColData
+        ,   selectCol : 'postId'
+        ,   counted : false
+        ,   title : 'FreeBoard List'
+        ,   maxData : 5
+        ,   colSpan : 4
+        ,   cellSelectEvent : (e) => {
+        }
+    }
+
+    useEffect(() => {
+        postSearch();
     },[]);
 
-    let tableInit = {
-        headerColName : ['No','제목','작성자','작성일']
-        ,   deleted : false
-        ,   colSpan : 4
+    const postSearch = () => {
+
+        const callBack = [
+            (result)=> {
+                setBodyNoticeData(result.data.postList);
+            },
+            (result)=> {
+                setBodyFreeBoardData(result.data.postList);
+            }
+        ]
+
+        let data = [{boardId    : "1"},{boardId    : "2"}];
+
+        data.forEach((value, index)=>{
+            common.fetchLoad("/postList","POST", value, callBack[index]);
+        })
     }
 
     return (
@@ -21,29 +65,19 @@ const  AdminMain = () => {
               <li className="breadcrumb-item active">Dashboard</li>
           </ol>
           <div className="row">
-              <AdminDashboardCard title={"공지사항"} bgColor={"card bg-primary text-white mb-4"} detailUrl={"/admin/post/1"}/>
-              <AdminDashboardCard title={"자유게시판"} bgColor={"card bg-success text-white mb-4"} detailUrl={"/admin/post/2"}/>
-              <AdminDashboardCard title={"이벤트"} bgColor={"card bg-warning text-white mb-4"} detailUrl={"/admin/post/3"}/>
-              <AdminDashboardCard title={"Q/A"} bgColor={"card bg-danger text-white mb-4"} detailUrl={"/admin/post/4"}/>
+              <BoardCard title={"공지사항"} bgColor={"card bg-primary text-white mb-4"} detailUrl={"/admin/post/1"}/>
+              <BoardCard title={"자유게시판"} bgColor={"card bg-success text-white mb-4"} detailUrl={"/admin/post/2"}/>
+              <BoardCard title={"이벤트"} bgColor={"card bg-warning text-white mb-4"} detailUrl={"/admin/post/3"}/>
+              <BoardCard title={"Q/A"} bgColor={"card bg-danger text-white mb-4"} detailUrl={"/admin/post/4"}/>
           </div>
           <div className="row">
               <div className="col-md-6">
-                  <div className="card-header">
-                      <i className="fas fa-table me-1"></i>
-                      Notice
-                  </div>
-                  <div className="card-body">
-                      {/*<Table tableInit={tableInit}></Table>*/}
-                  </div>
+                  <Table tableInit={tableInitNotice}
+                         bodyData={bodyNoticeData}/>
               </div>
               <div className="col-md-6">
-                  <div className="card-header">
-                      <i className="fas fa-table me-1"></i>
-                      FreeBoard
-                  </div>
-                  <div className="card-body">
-                      {/*<Table tableInit={tableInit}></Table>*/}
-                  </div>
+                  <Table tableInit={tableInitFreeBoard}
+                         bodyData={bodyFreeBoardData}/>
               </div>
           </div>
       </div>
