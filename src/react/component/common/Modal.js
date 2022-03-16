683b5f68ca4eb64579ca442c852db6d372ae196b
lgs0503 <tjrdl0503@naver.com>
2022-03-17 08:36:17 +0900

    [프론트] 모달 esc 종료 이벤트 추가

--- src/react/component/common/Modal.js
+++ src/react/component/common/Modal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../../css/modal.css';
 import '../../css/custom.css';
 
@@ -6,9 +6,19 @@ const Modal = (props) => {
     // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
     const { open, close, header } = props;
 
+    useEffect(() => {
+        const escKeyModalClose = (e) => {
+            if (e.keyCode === 27) {
+                close();
+            }
+        };
+        window.addEventListener("keydown", escKeyModalClose);
+        return () => window.removeEventListener("keydown", escKeyModalClose);
+    }, []);
+
     return (
         // 모달이 열릴때 openModal 클래스가 생성된다.
-        <div className={open ? 'openModal modal' : 'modal'}>
+        <div id="modal" className={open ? 'openModal modal' : 'modal'}>
             {open ? (
                 <section className={props.modalSize ? props.modalSize : 'modalSize4'}>
                     <header>
